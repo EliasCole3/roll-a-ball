931c5787c3dcf60bb57000849239a233ae86cb1c
Elias Cole <elias.cole@comcast.com>
2016-05-14 14:16:05 -0700

    5 of 8

---
+++ Roll-a-Ball/Assets/scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	void Update () 
+	{
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+//		transform.Rotate(new Vector3(15, 30, 45));
+//		transform.Rotate(new Vector3(15, 30, 45) * 0.01f);
+	}
+}
